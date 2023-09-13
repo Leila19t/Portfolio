c3b6eb84277d26bbc8c3731145711d73f8ef02ab
laila19t <127900249+laila19t@users.noreply.github.com>
2023-09-14 02:13:58 +0300

    added js projects

--- src/data.jsx
+++ src/data.jsx
@@ -1,6 +1,9 @@
 import vanlife from './assets/images/vanlife.png'
 import quizzical from './assets/images/quizzical.png'
 import travelJournal from './assets/images/travelJournal.png'
+import watchlist from './assets/images/watchlist.png'
+import dogsTinder from './assets/images/dogsTinder.png'
+import restaurant from './assets/images/restaurant.png'
 import {FaGraduationCap, FaGlobeEurope} from "react-icons/fa"
 import { SiScrimba } from "react-icons/si"
 
@@ -43,5 +46,26 @@ export const reactApps = [
         description: "Travel journal was a big help in learning how to use react components, and the concepts of composability and reusability.",
         link: "https://travel-journal-laila19t.netlify.app/",
         image: travelJournal
+    },
+]
+
+export const vanillaJSApps = [
+    {
+        name: "Movie Watchlist",
+        description: "Movie watch is a project that made me practice local storage, third party API calls, promises, and data manupilation.",
+        link: "https://movie-watchlist-laila19t.netlify.app/",
+        image: watchlist
+    },
+    {
+        name: "Tinder for dogs",
+        description: "I had a lot of laughs while making this project, but overall it really helped me understand the concept of classes and objects in JavaScript.",
+        link: "https://tindog-laila19t.netlify.app/",
+        image: dogsTinder
+    },
+    {
+        name: "Restaurant ordering app",
+        description: "Working with this small web app helped me learn all about event.target, dealing with forms, and having multiple reactive components on the screen.",
+        link: "https://restaurant-laila19t.netlify.app/",
+        image: restaurant
     },
 ]
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { reactApps } from "../data";
+import { vanillaJSApps } from "../data"
 import ProjectCard from "../components/ProjectCard";
+
 
 export default function Projects(){
 
     const reactAppElements = reactApps.map(app => <ProjectCard name={app.name} description={app.description} image={app.image} link={app.link}/>)
+    const JSAppElements = vanillaJSApps.map(app => <ProjectCard name={app.name} description={app.description} image={app.image} link={app.link}/>)
 
     return(
         <div className="projects-container">
@@ -15,7 +18,7 @@ export default function Projects(){
              </div>
              <h3>Featured <span className="green">Vanilla JavaScript projects</span>.</h3>
              <div className="inner-projects-container">
-                {reactAppElements}
+                {JSAppElements}
              </div>
         </div>
     )
