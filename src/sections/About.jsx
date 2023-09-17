26afb41723d69e1dc006b9102c4accb1993c4371
laila19t <127900249+laila19t@users.noreply.github.com>
2023-09-18 02:41:27 +0300

    added CV

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import aboutMe from "../assets/images/aboutMe.png"
 import { HiOutlinePaperClip } from "react-icons/hi";
 import { skills } from "../data";
+import CV from "../assets/CV.pdf"
 
 export default function About(){
 
@@ -17,7 +18,7 @@ export default function About(){
                     <p className="about-text">I am a fresh computer science graduate based out of Damascus, Syria. I am also fresh out of a six-month course dedicated to learn frontend web development, so you might say I'm all coded out! I consider myself a "forever student", because I'm fueled by learning new technologies and solving new problems in whatever shape or form they might come in.</p>
                     <p className="about-text">I also have a website or two that were made on Wordpress. these sites were projects that I have done while I was a foreign exchange student in Poland. I also worked as an IT assistant there for the summer which was very beneficial in terms of learning some new technical concepts. </p>
                     <p className="about-text">Right now, I am focusing on learning and completing as much projects as I can. But I'm always looking for a new challenge. Reach out to me at <a className="email" href="mailto:tarabih.laila@gmail.com?subject=Hello&body=Hello! let's discuss projects." target="_blank">tarabih.laila@gmail.com</a> and let's connect!</p>
-                    <button className="resume-btn">View resume <HiOutlinePaperClip/></button>
+                    <a className="resume-btn" href={CV} download="CV.pdf">View resume <HiOutlinePaperClip/></a>
                 </div>
              </div>
              <div className="technical-skills-container">
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { HashLink } from "react-router-hash-link";
 import { HiOutlinePaperClip } from "react-icons/hi";
+import CV from "../assets/CV.pdf"
+
 
 export default function Header() {
     return (
         <nav>
             <h3>laila19t<span className="green">.</span></h3>
             <div className="nav-links">
-                <HashLink to='#home' smooth>
+                <HashLink to='#home' smooth className="link">
                     Home
                 </HashLink>
-                <HashLink to='#projects' smooth>
+                <HashLink to='#projects' smooth className="link">
                     Projects
                 </HashLink>
-                <HashLink to='#about' smooth>
+                <HashLink to='#about' smooth className="link">
                     About me
                 </HashLink>
-                <HashLink to='#contact' smooth>
+                <HashLink to='#contact' smooth className="link">
                     Contact me
                 </HashLink>
-                <button className="resume-btn hidden">View resume <HiOutlinePaperClip/></button>
+                <a className="resume-btn hidden" href={CV} download="CV.pdf">View resume <HiOutlinePaperClip/></a>
             </div>
         </nav>
     )
